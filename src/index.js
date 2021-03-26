864542db6b06d6953f417913997cd65d3b6c4e24
Igor <ermygut@gmail.com>
2021-03-26 19:37:19 +0300

    increment decrement random

--- src/index.js
+++ src/index.js
@@ -4,18 +4,35 @@ import {createStore} from 'redux';
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC': return state +1;
+    case 'DEC': return state -1;
+    case 'RND': return state * action.value;
     default: return state;
   }
 }
 const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-})
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch({type: 'INC'});
+});
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch({type: 'DEC'});
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch({type: 'RND', value});
+});
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+ store.subscribe(update);
+
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
 
 
 
