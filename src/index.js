73ab94fa79e43dd4baa84c64e651eb11258ff118
Igor <ermygut@gmail.com>
2021-03-26 19:45:11 +0300

    store reducer actions

--- src/index.js
+++ src/index.js
@@ -6,23 +6,37 @@ const reducer = (state = 0, action) => {
     case 'INC': return state +1;
     case 'DEC': return state -1;
     case 'RND': return state * action.value;
+    case 'RES': return state = 0;
     default: return state;
   }
 }
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', value} );
+const res = () => ({type: 'RES'});
+
+
 const store = createStore(reducer);
 
 document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch({type: 'INC'});
+  store.dispatch(inc());
 });
 
 document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch({type: 'DEC'});
+  store.dispatch(dec());
 });
 
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  store.dispatch({type: 'RND', value});
+  store.dispatch(rnd(value));
 });
+
+document.getElementById('res').addEventListener('click', () => {
+  store.dispatch(res());
+});
+
+
 
 const update = () => {
   document.getElementById('counter').textContent = store.getState();
